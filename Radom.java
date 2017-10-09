cd4591cf4a37bde24dff23b618f101a920f09b74
koutsomoura <maria_karaxatzi@hotmail.com>
2017-10-09 23:40:36 +0200

    i askisi

---
+++ Radom.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+
+public class Radom {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 double[] Arr = new double[10];
+
+		 for (int i = 0; i < Arr.length; i++) {
+			 
+			 
+			 O protos tropos 
+			 /*Random rand = new Random(); 
+			 int value = rand.nextInt(10);
+			 System.out.println(value);
+			 */
+			 
+			 
+			 O deuteros tropos
+			 /*System.out.println((int)(Math.random()*10));
+			  * 
+			  */
+
+		}
+}
+}
