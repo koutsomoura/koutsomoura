b7d0d20518239032494d81229a6f455d82b93078
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-05 17:42:29 +0100

    a

--- exercise2.java
+++ exercise2.java
@@ -8,31 +8,27 @@ public class exercise2 {
 		// TODO Auto-generated method stub
 		int a;
 		int b;
-		a=4+(int)(Math.random()*100);
-		b=4+(int)(Math.random()*100);
+		a=4+(int)(Math.random()*97);
+		b=4+(int)(Math.random()*97);
 		if(a!=b){
 
-		System.out.println("a="+a+"    b="+b);
-			if (a>b){
-				
-			}
-			else if (a<b){
-				int temp;
-				temp=a;
+				if (a<b){
+					int temp;
+					temp=a;
+					a=b;
+					b=temp;
+					System.out.println("a="+a+"  b="+b);
+				}
+			
+		int z;
+		do{
+				z=a%b;
 				a=b;
-				b=temp;
-				System.out.println("a="+a+"  b="+b);
-			}
+				b=z;
+		}while (z==0);
+		System.out.println(b);
 			
-			int z;
-			do{
-			z=a%b;
-			a=b;
-			b=z;
-			}while (z==0);
-			System.out.println(b);
-			
-	}
+		}
 		
 	}
 }
