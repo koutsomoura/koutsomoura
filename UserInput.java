44d6a333b1841e74b31022c929c73dbed2b97a86
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-10 00:07:59 +0100

    diorthosi

--- UserInput.java
+++ UserInput.java
@@ -3,11 +3,17 @@ public class UserInput {
 	
 	
 
-	public String getString()throws IOException{
+	public static String getString()throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int name=br.readLine();
+		String name=br.readLine();
 		return name;
 	} 
-	
+	public static double getInteger(){
+		int meresergasias=Integer.parseInt(br.readLine());
+		double hmeromosthio=Double.parseDouble(br.readLine());
+		double yperories=Double.parseDouble(br.readLine());
+		
+		return meresergasias,hmeromosthio,yperories;
+	}
 	
 }
