0eecdfa635669fa788867bbe938881696181c59a
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-11 00:13:19 +0100

    a

--- askisi5.java
+++ askisi5.java
@@ -1,26 +1,24 @@
 
-public class askisi5 {
+class askisi5<T>{
+	
+	private T t;
 
-	/**
-	 * @param args
-	 */
+	public void add(T t) {this.t= t;}
+	
+	public T get() {return t;}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		class GenericTest1<T>{
-		private T t;
-		public void add(T t) {this.t= t;}
-		public T get() {
-		return t;}
-		public static void main(String[] args) {
 			
-			GenericTest1<Integer> i1 = new GenericTest1<Integer>();
-			GenericTest1<String> s1 = new GenericTest1<String>();
-			i1.add(new Integer(25));
-			s1.add(new String("Hello Generic Class"));
-			System.out.println("Arithmos = " + i1.get());
-			System.out.println("To string = " + s1.get());
-		}
-		}
+		GenericTest1<Integer> i1 = new GenericTest1<Integer>();
+		GenericTest1<String> s1 = new GenericTest1<String>();
+		
+		i1.add(new Integer(25));
+		s1.add(new String("Hello Generic Class"));
+		
+		System.out.println("Arithmos = " + i1.get());
+		System.out.println("To string = " + s1.get());
+		
+		
 	}
 
 }
