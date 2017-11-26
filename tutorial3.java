0e79505c3a19d95458ba8bb4ff1410d8bd6b246b
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-26 12:40:32 +0100

    s

---
+++ tutorial3.java
@@ -0,0 +1,18 @@
+public class tutorial3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int    i     =    0;
+		Person person1   =    new Person(15,83,"tsoutsouli","kara");
+		person1.surname="kara";
+		person1.h=15;
+		person1.w=83;
+
+		
+		System.out.println(person1.toString());
+		
+	}
+
+}
