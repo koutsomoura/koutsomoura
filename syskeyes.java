d4fc6eaaae79a0295b39c5f58c369050344bb854
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-26 18:09:52 +0100

    adw

--- syskeyes.java
+++ syskeyes.java
@@ -3,6 +3,7 @@ public class syskeyes {
 
 	final String ypokatasthma="Ypokatasthma Thessalonikis";
 	private int arithmosSyskeyhs;
+	private int kodikosSyskeyhs;
 	private double arxikhTimhSyskeyhs;
 	private double telikhTimhSyskeyhs;
 	
@@ -21,11 +22,18 @@ public class syskeyes {
 		
 	}
 	
-	public static int getArithmosSyskeyhs(int arithmosSyskeyhs){
-		return arithmosSyskeyhs=arithmosSyskeyhs;
+	public void setArithmosSyskeyhs(int arithmosSyskeyhs){
+		this.arithmosSyskeyhs=arithmosSyskeyhs;
 	}
-	public static double getArxikhTimhSyskeyhs(double arxikhTimhSyskeyhs){
-		return arxikhTimhSyskeyhs=arxikhTimhSyskeyhs;
+	public void setKodikosSyskeyhs(int kodikosSyskeyhs){
+		this.kodikosSyskeyhs=kodikosSyskeyhs;
+	}
+	public void setArxikhTimhSyskeyhs(double arxikhTimhSyskeyhs){
+		this.arxikhTimhSyskeyhs=arxikhTimhSyskeyhs;
+	}
+	public String getYpokatasthma(String ypokatasthma){
+		ypokatasthma=ypokatasthma;
+		return ypokatasthma;
 	}
 	public static void getTelikhTimhSyskeyhs(double syntelestis,double telikhTimhSyskeyhs){
 		double ektosi=(syntelestis/100)*telikhTimhSyskeyhs;
