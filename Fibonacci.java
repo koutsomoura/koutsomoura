00024d3fd95a849bf48c92425a1fce35b6281108
koutsomoura <maria_karaxatzi@hotmail.com>
2017-11-27 19:01:52 +0100

    nes

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+
+
+public class Fibonacci {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		classofstaticmethods a[]=new classofstaticmethods[10];
+		int x[]=new int [10];
+		x=classofstaticmethods.fillPinInt(a);
+		for (int i=0;i<10;i++){
+			System.out.println(x);}
+		
+	}
+}
--- classofstaticmethods.java
+++ classofstaticmethods.java
@@ -1,30 +1,35 @@
 
 public class classofstaticmethods {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		static int trexonArithmosSyskeyhs=1;
+	
+	static int trexonArithmosSyskeyhs=1;
+	
+	public static void[] fillPinInt(int a[]){
+		for (int index=0;index<a.length;index++){
+			for (int i=0;i<a.length;i++){
+				a[0]=1;
+				a[i]=a[i-1]+a[i];
+				System.out.println(a[i]);
+				System.out.println();
 		
-	}
-	public static void fillPinInt(int Fibonacci[]){
-		Fibonacci[i]=Fibonacci???????
-	}
-	public static double findSynolikoPosoPlhromhs(){
-		
-		if (int n!=0){
-			n=
-			synolo=n*arxikhTimhSyskeyhs.getArxikhTimhSyskeyhs();
-			if (n*arxikhTimhSyskeyhs){
-				
 			}
 		}
+	 }
+	public static double findSynolikoPosoPlhromhs(double telikhTimhSyskeyhs){
+		syskeyes p= new syskeyes();
+		double k=p.getArxikhTimhSyskeyhs();
+		double sinolo=0;
+		if(trexonArithmosSyskeyhs>=1&&k>2000){
+			sinolo=p.getTelikhTimhSyskeuhs();
+			sinolo=sinolo-(sinolo*(30.0/100.0));
 			
-		
-		
-		double telikoposo
+		}
+		telikhTimhSyskeyhs=sinolo;
+		return telikhTimhSyskeyhs;
 	}
-
+		
 }
+
+		    
+	
+
